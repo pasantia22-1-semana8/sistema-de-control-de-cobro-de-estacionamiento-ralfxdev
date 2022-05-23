1107439e674a343f2c94be97747f78faeb5cb34d
ralfxdev <lopezalexander1019@gmail.com>
2022-05-23 10:57:43 -0600

    add app

---
+++ frontend/src/App.js
@@ -0,0 +1,68 @@
+/* React */
+import React from "react";
+import { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+/* Components */
+import Nav from "./components/Nav";
+
+/* Pages */
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+
+/* Services */
+import { getData } from "./services/Api";
+
+/* Styles */
+import "./App.css";
+
+/* Si el usuario ha iniciado sesión, muestrea el dashboard; de lo contrario, muestra la página de inicio de sesión */
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+
+  componentDidMount() {
+    getData("user").then((data) => {
+      if (data.name) {
+        this.setState({ name: data.name });
+      }
+    });
+  }
+
+  render() {
+    if (this.state.name) {
+      return (
+        /* Rutas Privadas */
+        <Router>
+          <Nav name={this.state.name} />
+          <Routes>
+            <Route path="/" element={<Dashboard name={this.state.name} />} />
+            <Route
+              path="ajustes/register"
+              element={<Register name={this.state.name} />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      );
+    } else {
+      return (
+        /* Rutas Públicas */
+        <Router>
+          <Nav name={this.state.name} />
+          <Routes>
+            <Route path="*" element={<Login name={this.state.name} />} />
+          </Routes>
+        </Router>
+      );
+    }
+  }
+}
+
+export default App;
